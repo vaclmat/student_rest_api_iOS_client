4612edd117c7818f3cb40bce1ffbc6d75866acc3
Vaclav Matousek <vaclmat@email.cz>
2019-05-13 02:54:56 -0700

    Initial Commit

---
+++ Student Database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Student Database
+//
+//  Created by Vaclav Matousek on 13/05/2019.
+//  Copyright © 2019 Vaclav Matousek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Student DatabaseTests/Student_DatabaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Student_DatabaseTests.swift
+//  Student DatabaseTests
+//
+//  Created by Vaclav Matousek on 13/05/2019.
+//  Copyright © 2019 Vaclav Matousek. All rights reserved.
+//
+
+import XCTest
+@testable import Student_Database
+
+class Student_DatabaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
